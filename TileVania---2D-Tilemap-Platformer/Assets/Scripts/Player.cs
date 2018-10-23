b61bcda4f8df9f2c5ccaba9af8493315ad400550
Nannings <mrnannings@gmail.com>
2018-10-23 09:16:29 +0200

    312;

--- TileVania---2D-Tilemap-Platformer/Assets/Scripts/Player.cs
+++ TileVania---2D-Tilemap-Platformer/Assets/Scripts/Player.cs
@@ -6,11 +6,16 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5;
+
+    bool isAlive = true;
+
     Rigidbody2D rigidbody2D;
+    Animator animator;
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -24,6 +29,9 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidbody2D.velocity.y);
         rigidbody2D.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("Running", playerHasHorizontalSpeed);
     }
 
     private void FlipSprite()
